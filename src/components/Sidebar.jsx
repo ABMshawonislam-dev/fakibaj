0b67f6d1ca6d9f766f04115071e11bb7fa7dd0cb
devfdr2301 <creativeitstudents2301@gmail.com>
2023-11-09 15:29:56 +0600

    crop

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,72 @@
-import React ,{useState}from 'react'
+import React ,{useState,useRef,createRef}from 'react'
 import { useSelector } from 'react-redux';
 import {AiFillHome,AiFillMessage,AiFillSetting,AiOutlineLogout} from "react-icons/ai"
 import {IoIosNotifications} from "react-icons/io"
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+
+const defaultSrc =
+  "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 const Sidebar = () => {
     let userInfo = useSelector((state)=>state.logedUser.value)
 
 let [url,setUrl] = useState("home")
+const [open, setOpen] = React.useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
+
+const [image, setImage] = useState(defaultSrc);
+  const [cropData, setCropData] = useState("#");
+  const cropperRef = createRef()
+
+  const onChange = (e) => {
+    e.preventDefault();
+    let files;
+    if (e.dataTransfer) {
+      files = e.dataTransfer.files;
+    } else if (e.target) {
+      files = e.target.files;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
+  const getCropData = () => {
+    if (typeof cropperRef.current?.cropper !== "undefined") {
+      setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+    }
+  };
+
+
+
+
   return (
     <div className='sidebar'>
+        <Button onClick={handleOpen}>
+
         <img className='sidebarimg' src={userInfo.photoURL}/>
+        </Button>
         <h1>{userInfo.displayName}</h1>
 
         <ul >
@@ -26,7 +82,42 @@ let [url,setUrl] = useState("home")
             <li><AiFillSetting className='icon'/></li>
             <li><AiOutlineLogout className='icon'/></li>
         </ul>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+            <div className="img-preview"></div>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <input type="file" onChange={onChange} />
+            <Cropper
+                ref={cropperRef}
+                style={{ height: 400, width: "100%" }}
+                zoomTo={0.5}
+                initialAspectRatio={1}
+                preview=".img-preview"
+                src={image}
+                viewMode={1}
+                minCropBoxHeight={10}
+                minCropBoxWidth={10}
+                background={false}
+                responsive={true}
+                autoCropArea={1}
+                guides={true}
+            />
+     
+            
+        
+          </Typography>
+        </Box>
+      </Modal>
     </div>
+  
   )
 }
 
